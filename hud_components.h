dffd5cf5be3c0f5b7f44436df095f3eafd51b381
Teddy Walsh <teddyjwalsh@gmail.com>
2020-07-28 10:36:37 -0500

    Started consctruction of HudElements

--- hud_components.h
+++ hud_components.h
@@ -1,8 +1,21 @@
 #ifndef HUD_COMPONENTS_H_
 #define HUD_COMPONENTS_H_
 
+
+struct HudElement
+{
+    HudObject * object;
+    std::vector<HudElement*> children;
+
+    template <class T>
+    void attach(T * val)
+    {
+    }
+};
+
 class CompHudElement : public Component
 {
+    HudElement hud;
 };
 
 #endif  // HUD_COMPONENTS_H_
